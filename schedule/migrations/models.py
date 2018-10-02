6f4467cbe29ee64979030c9fb3fcdc7703edcc99
makimov <daggerito@gmail.com>
2018-10-02 16:10:30 +0300

    Adds ForeignKeys

--- schedule/migrations/models.py
+++ schedule/migrations/models.py
@@ -1,36 +1,40 @@
-class Pupil():
+from django.db import models
+
+
+class Pupil(models.Model):
     lastname = models.CharField(max_lenght=32)
     firstname = models.CharField(max_lenght=32)
     surname = models.CharField(max_lenght=32)
     grade = models.CharField(max_lenght=4)
 
 
-class Teacher():
+class Teacher(models.Model):
     lastname = models.CharField(max_lenght=32)
     firstname = models.CharField(max_lenght=32)
     surname = models.CharField(max_lenght=32)
 
 
-class Grade():
+class Grade(models.Model):
     name = models.CharField(max_lenght=4)
-    lead = models.SmallIntegerFiled() # Teacher.id
+    lead = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
 
 
-class Room():
-    number = models.SmallIntegerFiled(max_lenght=3)
+class Room(models.Model):
+    number = models.PositiveIntegerField(default=0)
+    owner = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
 
 
-class Subject():
+class Subject(models.Model):
     name = models.CharField(max_lenght=32)
-    teacher = models.SmallIntegerFiled()  # Teacher.id
+    teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
 
 
-class Lesson():
-    grade = models.SmallIntegerFiled() # Grade.id
-    subject = models.SmallIntegerFiled() # Subject.id
-    room = models.SmallIntegerFiled() # Room.id
+class Lesson(models.Model):
+    grade = models.ForeignKey('Grade', on_delete=models.SET_NULL, null=True)
+    subject = models.ForeignKey('Subject', on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
 
 
-class Schedule():
-    lesson = models.SmallIntegerFiled() # Lesson.id
+class Schedule(models.Model):
+    lesson = models.ForeignKey('Lesson', on_delete=models.SET_NULL, null=True)
     datetime = models.DateTimeField()
