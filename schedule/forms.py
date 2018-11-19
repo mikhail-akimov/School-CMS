f31718dd14fed8749fd430ab7ad36bd4182b1277
makimov <daggerito@gmail.com>
2018-11-19 17:26:55 +0300

    Validation compleate.

--- schedule/forms.py
+++ schedule/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext as _
 
 class AddGrade(forms.ModelForm):
     name = forms.CharField(help_text="Enter class name", max_length=3, min_length=2,
-                           error_messages={'required': 'Укажите логин'})
+                           error_messages={'required': 'Укажите логин', 'char': 'Укажите букву класса'})
 
     class Meta:
         model = Grade
@@ -15,5 +15,6 @@ class AddGrade(forms.ModelForm):
         name = self.cleaned_data['name']
 
         if name[-1].isdigit():
-            raise ValueError(_('Please enter grade char.'))
+            raise forms.ValidationError(_('Error!'))
         return name
+
