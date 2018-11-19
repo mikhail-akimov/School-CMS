c3dcaaba6a5e6b6f64f678dba01788efb1de3bf2
makimov <daggerito@gmail.com>
2018-11-19 12:50:27 +0300

    Validation added, but not works.

--- schedule/forms.py
+++ schedule/forms.py
@@ -4,17 +4,22 @@ from .models import Grade
 
 class AddGrade(forms.ModelForm):
     name = forms.CharField(help_text="Enter class name")
+    model = Grade
+    fields = ('name', 'lead')
 
-    def clean_grade_name(self):
+    def clean_name(self):
         name = self.cleaned_data['name']
+        print(name)
+        print(name[-1])
 
         if len(name) > 3:
-            raise ValueError(_('Grade name is too long!'))
+            raise ValueError('Grade name is too long!')
 
-        if len(name) < 2:
-            raise ValueError(_('Grade name is too short!'))
+        elif len(name) < 2:
+            raise ValueError('Grade name is too short!')
 
-        if name[-1] in range(0, 9):
-            raise ValueError(_('Please enter grade char.'))
+        elif name[-1].isdigit():
+            raise ValueError('Please enter grade char.')
 
-        return name
+        else:
+            return name
